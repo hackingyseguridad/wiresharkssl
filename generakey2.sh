4bda64ee9336ca3ab428d6c7601785e6792ed878
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2017-11-19 22:39:05 +0100

    Update generakey2.sh

--- generakey2.sh
+++ generakey2.sh
@@ -2,4 +2,4 @@
 
 openssl pkcs12 in certificado.pfx -out claveprivada.pem -nocerts -nodes
 # Eliminamos la passphrase de la clave privada 
-openssl rsa -in claveprivada.pem -out claveprivadasinpass.pem
+openssl rsa -in claveprivada.pem -out key.pem
