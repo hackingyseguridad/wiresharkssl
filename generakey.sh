faa054a60367c263baaa73f1175fabf4dab6e10f
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2017-11-19 23:09:02 +0100

    Update generakey.sh

--- generakey.sh
+++ generakey.sh
@@ -3,4 +3,5 @@ echo
 echo "Se generara un fichero cert.pem con certificado SSL/TLS y un fichero key.pem con la clave privada."
 echo
 openssl req -x509 -nodes -newkey rsa:1024 -keyout key.pem -out cert.pem
-echo 
+echo "Eliminamos la passphrase de la clave privada" 
+openssl rsa -in key.pem -out key2.pem
