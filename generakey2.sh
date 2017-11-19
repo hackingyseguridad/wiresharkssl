fef2bb8176667dd44ce1fb66efbb8911828a97fa
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2017-11-19 23:09:15 +0100

    Delete generakey2.sh

--- generakey2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-openssl pkcs12 in certificado.pfx -out claveprivada.pem -nocerts -nodes
-# Eliminamos la passphrase de la clave privada 
-openssl rsa -in claveprivada.pem -out key.pem
